1fd1e29b1f71c42b03ef56d27f44f20bede01750
Annrio16 <annrio1606@gmail.com>
2022-06-29 22:43:52 +0530

    start

---
+++ first.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html >
+    <title>
+        Tutor Management
+    </title>
+    <head>
+        <link rel="stylesheet" href="first.css">
+        <div class="common">
+            <div class="sub">
+                <h2>Leave Management System</h2>
+            </div>
+            <div class="box">
+                <div class="card">
+                    <i class="fa-solid fa-file-certificate"></i>
+                    <h5>HOD</h5>
+                    <p style="text-align: center;">
+                        <a class="button" href="http://localhost:3000/admin/index1.php">Login</a></p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="card">
+                    <i class="fa-solid fa-clipboard-user"></i>
+                    <h5>Tutor</h5>
+                    <p style="text-align: center;">
+                        <a class="button" href="http://localhost:3000/admin/index.php">Login</a></p>
+                </div>
+                
+        </div>
+        <div class="box">
+            <div class="card">
+                <i class="fa-solid fa-square-check"></i>
+                <h5>Student</h5>
+                <p style="text-align: center;">
+                    <a class="button" href="index.php">Login</a></p>
+            </div>
+    </head>
+    <body >
+        
+    </body>
+</html>
